bf0f98a19f126d42a60761ca58ab1163a88b21fc
UmarSiddiqui <umarsiddiqui3037@gmail.com>
2023-10-03 10:57:00 +0800

    Add project files.

---
+++ ExpenseAPI/Models/SharedExpense.cs
@@ -0,0 +1,16 @@
+﻿namespace ExpenseAPI.Models
+{
+    public class SharedExpense
+    {
+
+        public int ID { get; set; }
+        public string ExpenseName { get; set; }
+        public string ExpenseDescription { get; set; }
+        public float ExpenseAmount { get; set; }
+        public SharedExpense()
+        {
+
+
+        }
+    }
+}
